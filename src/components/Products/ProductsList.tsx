bf2d43283fcf752c4da0e1b290ff025a98a38b85
isaiahsilvani <isaiahsilvani@outlook.com>
2021-05-31 10:52:43 -0400

    initial commit

---
+++ src/components/Products/ProductsList.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+interface ProductsListProps {
+
+}
+
+const ProductsList: React.FC<ProductsListProps> = ({}) => {
+    return (
+      <div>
+        <label htmlFor="">
+          Games List
+        </label>
+      </div>
+    );
+}
+
+export default ProductsList
